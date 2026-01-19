75cb8d438210cda67ae83d2c8fe105a091c734c2
drdoom0979-dot <drdoom0979@gmail.com>
2026-01-19 15:23:54 -0600

    feat: initial project setup

---
+++ src/test/java/com/drdoom/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drdoom.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
